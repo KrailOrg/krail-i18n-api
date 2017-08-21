dd444417d7a0d66a067b63e147cb6c00cdfb5aaf
David Sowerby <david.sowerby@virgin.net>
2017-08-21 19:55:34 +0100

    Updated persist-api, added MockLabelKey

---
+++ src/main/java/uk/q3c/krail/i18n/test/MockLabelKey.java
@@ -0,0 +1,10 @@
+package uk.q3c.krail.i18n.test;
+
+import uk.q3c.krail.i18n.I18NKey;
+
+/**
+ * Created by David Sowerby on 21 Aug 2017
+ */
+public enum MockLabelKey implements I18NKey {
+    Yes, No, Cancel
+}
